578a0b3a699b08dbbf2a88d7563d82620decba20
Jake Dibble <jdibble@bsu.edu>
2020-10-13 14:02:03 -0400

    Add enemy

---
+++ project/src/Enemy.gd
@@ -0,0 +1,8 @@
+extends KinematicBody2D
+
+
+
+func _ready():
+	$AnimatedSprite.play("idle")
+
+
--- project/src/PlayerBullet.gd
+++ project/src/PlayerBullet.gd
@@ -1,12 +1,18 @@
 extends Area2D
 
-const SPEED = 600
-
-var _vel := Vector2()
-var _x_coordinate = 0
+const SPEED := 600
 
 func _ready():
 	pass
 	
 func _physics_process(delta):
 	position += transform.y * -SPEED * delta
+	if position.y <= 0:
+		queue_free()
+		print("offscreen deleted!")
+		
+
+
+func _on_PlayerBullet_body_entered(body):
+	queue_free()
+	print("HIT!")
