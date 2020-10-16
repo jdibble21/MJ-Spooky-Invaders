4bfb74288f680ee2987c0ac78a75261cd1fa1cbb
Jake Dibble <jdibble@bsu.edu>
2020-10-16 14:58:30 -0400

    Add HUD elements

---
+++ project/src/HUD.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+
+var _time = 0.0
+var rounded_time
+
+onready var _time_label := $TimeLabel
+
+func _process(delta):
+	_time += delta
+	rounded_time = int(_time)
+	_time_label.text = "TIME: " + str(rounded_time)
--- project/src/Level.gd
+++ project/src/Level.gd
@@ -3,13 +3,17 @@ extends Node2D
 onready var _player = $Player
 onready var _spawn_points = $SpawnPoints
 onready var _music_loop = $MusicLoop
-
+onready var _HUD_lives_label = $HUD/LivesLabel
 func _ready():
 	# evenutally place fly in animation for enemy spawning here
+	_player.connect("player_defeated",self,"_game_over")
 	_music_loop.play()
 	pass
 	
 func _process(delta):
-	pass
+	_HUD_lives_label.text = "LIVES: " + str($Player.lives)
 
-
+func _game_over():
+	set_process(false)
+	_HUD_lives_label.text = "LIVES: 0" 
+	_music_loop.stop()
